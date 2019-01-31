3bce179eca31c58a55bc726287f16fa5da521c46
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-01-31 19:39:27 +0200

    Initial Commit

---
+++ TextFieldWhithTouchBegan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldWhithTouchBegan
+//
+//  Created by Артем on 1/31/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
